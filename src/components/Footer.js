2ebe72c34d54a21304d51bec9f8848b66650c55f
Artem Lyubchuk <artemlyubchuk@mail.ru>
2019-12-22 15:43:36 +0300

    Preparation for version 2

--- src/components/Footer.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export const Footer = props => (
-  <footer>Контакты, и пасхалка (звучит интригующе)</footer>
-)
--- src/pages/cupboard.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { Layout } from "../components/Layout"
-import { Intro } from "../components/Intro"
-
-export default () => (
-  <Layout fullWidth>
-    <Intro>Интересное, ценное и полезное. По полочкам.</Intro>
-  </Layout>
-)
