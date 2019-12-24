59ce906ee8a5412c5b829e6e339616a718a7e521
Artem Lyubchuk <artemlyubchuk@mail.ru>
2019-12-24 19:09:04 +0300

    Add default values for optional fields

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@ const moment = require("moment")
 const siteConfig = require("./data/SiteConfig")
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  // main goal of this plugin is to transform url of each node from any_Case to kebab-case
+  // transform url of each node from any_Case to kebab-case
   const { createNodeField } = actions
   let slug
   if (node.internal.type === "MarkdownRemark") {
@@ -30,9 +30,9 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
         slug = `/${_.kebabCase(node.frontmatter.slug)}`
       if (Object.prototype.hasOwnProperty.call(node.frontmatter, "date")) {
         const date = moment(node.frontmatter.date, siteConfig.dateFromFormat)
-        if (!date.isValid)
+        if (!date.isValid) {
           console.warn(`WARNING: Invalid date.`, node.frontmatter)
-
+        }
         createNodeField({ node, name: "date", value: date.toISOString() })
       }
     }
