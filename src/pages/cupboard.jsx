c0c01e78e47882f8d983063bdb39f0bb275be0b3
Artem Lyubchuk <artemlyubchuk@mail.ru>
2020-01-06 14:48:18 +0300

    Cupboard data schema

--- src/pages/cupboard.jsx
+++ src/pages/cupboard.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from "react"
+import Helmet from "react-helmet"
 import Intro from "../components/Intro"
 import Layout from "../components/Layout"
+import config from "../../data/SiteConfig"
 
 export default class Cupboard extends Component {
   render() {
     return (
       <Layout fullWidth>
+        <Helmet title={`Шкаф | ${config.siteTitle}`} />
         <Intro>
           Ни для кого не секрет (это ирония, возможно для кого-то и секрет), что
           всё, что нас окружает, формирует наше поведение. И это нормально.
           Здесь складываются видосы, статьи, заметки, картинки, которые меня
           поддёрнули. Основная цель &mdash; уловить смену интересов. А чё,
-          занятно будет глянуть через время, как менялся фокус.
+          занятно будет глянуть, как менялся фокус.
         </Intro>
       </Layout>
     )
