32ebc25c29cdfbeb75fe2f4211b9dbc7e19e540b
Artem Lyubchuk <artemlyubchuk@mail.ru>
2020-01-14 14:40:20 +0300

    Use relative path inside md post

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,9 @@ module.exports = {
       options: {
         plugins: [
           {
+            resolve: "gatsby-remark-relative-images",
+          },
+          {
             resolve: "gatsby-remark-images",
             options: {
               maxWidth: 860,
