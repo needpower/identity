44eb419d5c8771929163fedcf185238c72e15606
Artem Lyubchuk <artemlyubchuk@mail.ru>
2020-02-04 22:48:41 +0300

    Mobile menu: init and animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,91 @@
-import React from "react"
+import React, { useState, useCallback } from "react"
 import { Link } from "gatsby"
 import styled from "@emotion/styled"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons"
 import { options } from "../utils/typography"
+import { phone } from "../utils/mediaQueries"
 
-const HeaderComponent = ({ className }) => (
-  <HeaderTag className={className}>
-    <LogoWrapper>
-      <Logo to="/">needpower</Logo>
-    </LogoWrapper>
-    <Nav>
-      <NavLink to="/notes" partiallyActive activeClassName="active">
-        Говорить
-      </NavLink>
-      <NavLink to="/projects" partiallyActive activeClassName="active">
-        Мешки ворочать
-      </NavLink>
-      <NavLink to="/cupboard" activeClassName="active">
-        Шкаф
-      </NavLink>
-    </Nav>
-  </HeaderTag>
-)
+function HeaderComponent({ className }) {
+  const [isMenuOpened, toggleMenu] = useState(false)
+  const toggleMobileMenu = useCallback(() => toggleMenu(!isMenuOpened), [
+    isMenuOpened,
+    toggleMenu,
+  ])
+  return (
+    <>
+      <HeaderTag className={className}>
+        <LogoWrapper>
+          <MobileMenuTrigger onClick={toggleMobileMenu}>
+            <FontAwesomeIcon
+              icon={faAngleDoubleRight}
+              size="1x"
+              rotation={isMenuOpened ? 180 : null}
+            />
+          </MobileMenuTrigger>
+          <Logo to="/">needpower</Logo>
+        </LogoWrapper>
+      </HeaderTag>
+      <Nav isMenuOpened={isMenuOpened}>
+        <NavLink to="/notes" partiallyActive activeClassName="active">
+          Говорить
+        </NavLink>
+        <NavLink to="/projects" partiallyActive activeClassName="active">
+          Мешки ворочать
+        </NavLink>
+        <NavLink to="/cupboard" activeClassName="active">
+          Шкаф
+        </NavLink>
+      </Nav>
+    </>
+  )
+}
 
 const HeaderTag = styled.header`
   font-family: ${options.headerFontFamily.join()};
+  height: 48px;
   margin: 16px auto 0;
   max-width: 1280px;
+  position: relative;
+  ${phone} {
+    width: 100%;
+    z-index: 2;
+  }
+`
+
+const MobileMenuTrigger = styled.span`
+  color: rgba(97, 98, 71, 1);
+  cursor: pointer;
+  display: none;
+  left: 40px;
+  position: absolute;
+  ${phone} {
+    display: inline-block;
+  }
 `
 
 const Nav = styled.nav`
   align-items: center;
   display: flex;
-  justify-content: center;a
+  font-family: ${options.headerFontFamily.join()};
+  justify-content: center;
+  transform: translateX(0);
+  transition: transform 0.5s cubic-bezier(0.55, 0, 0.1, 1);
+  ${phone} {
+    align-items: flex-start;
+    background-color: #ffffff;
+    flex-direction: column;
+    justify-content: flex-start;
+    height: 100%;
+    left: 0;
+    padding: 72px 16px 0 24px;
+    position: absolute;
+    top: 0;
+    transform: ${({ isMenuOpened }) =>
+      isMenuOpened ? "translateX(0)" : "translateX(-310px)"};
+    width: 320px;
+    z-index: 1;
+  }
 `
 
 const NavLink = styled(Link)`
@@ -45,6 +99,9 @@ const NavLink = styled(Link)`
   position: relative;
   text-decoration: none;
   text-shadow: inherit;
+  ${phone} {
+    margin-bottom: 8px;
+  }
   &:hover {
     color: rgba(25, 123, 189, 1);
   }
@@ -73,7 +130,11 @@ const NavLink = styled(Link)`
 
 const LogoWrapper = styled.div`
   margin-bottom: 16px;
+  position: relative;
   text-align: center;
+  ${phone} {
+    margin-bottom: 0;
+  }
 `
 
 const Logo = styled(Link)`
